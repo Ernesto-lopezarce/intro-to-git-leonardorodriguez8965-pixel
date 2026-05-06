e6e81b91f2334b73f391adc197268d769c431539
usuario <leonardo.rodriguez8965@alumnos.udg.mx>
2026-05-06 08:46:21 -0600

    tercer commit

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int main() {
+
+    int edad;
+    float promedio;
+    int reprobadas;
+    int faltas;
+
+    printf("Edad: ");
+    scanf("%d", &edad);
+
+    printf("Promedio: ");
+    scanf("%f", &promedio);
+
+    printf("Materias reprobadas: ");
+    scanf("%d", &reprobadas);
+
+    printf("Faltas acumuladas: ");
+    scanf("%d", &faltas);
+  
+    // hola,
+    
+    return 0;
+}
